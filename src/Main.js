2417b7f4ee22d0f2507e7d8961432f8a6644d9d0
Rudra <rudra4365@gmail.com>
2021-09-11 00:37:41 +0530

    added standings option

---
+++ src/Main.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from "react"
+import "./Main.css"
+
+function Main() {
+
+    const [contests, setcontests] = useState([]);
+
+    useEffect(() => {
+        fetch("https://codeforces.com/api/contest.list?gym=false")
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            setcontests(data.result);
+        })
+        
+    }, [])
+    console.log(contests);
+    return (
+        <div className = "allContests">
+            <h1>
+                Codeforces Contests
+            </h1>
+            <div >
+                    {
+                        contests.map(contest => (
+                            <div  className = "indi">
+                            {
+                                <a href = {`http://codeforces.com/contest/${contest.id}/standings`}>
+                                <button className = "rt">Standings</button>
+                                </a>
+                            }
+                            <div>{contest.name}</div>
+                            </div>
+                            ))
+                    }
+            </div>
+        </div>
+    )
+}
+
+export default Main;
